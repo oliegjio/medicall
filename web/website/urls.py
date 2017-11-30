74451e2ca0edab6e55f7c23920aaaa595a3e55cd
oliegjio <oliegjio@gmail.com>
2017-11-30 15:37:25 +0000

    Commit

--- web/website/urls.py
+++ web/website/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^signup$', views.signup, name='signup'),
-    # url(r'^new-user$', views.new_user, name='new-user')
-    url(r'^accounts/', include('django.contrib.auth.urls'))
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^signup$', views.signup, name='signup')
 ]
--- web/website/views.py
+++ web/website/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
 def index(request):
     return render(request, 'home.html')
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'registration/signup.html', {'form': form})
 
