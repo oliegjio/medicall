098a023d1fd9277ddfdce5a0fa86a23326c9c2a8
oliegjio <oliegjio@gmail.com>
2017-12-08 03:01:33 +0000

    Commit

--- web_node/routes/user.js
+++ web_node/routes/user.js
@@ -3,7 +3,6 @@ const auth = require('../app/auth.js')()
 
 var init = (app) => {
   app.post('/user', auth.authenticate(), (request, response) => {
-    // response.json(users[request.user.id])
     response.json("true")
   })
 }
